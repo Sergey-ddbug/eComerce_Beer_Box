f1efa0da815f89f03c331b844373c76cb521af9c
Sergey Ishimov <sishimov@gmail.com>
2021-05-01 11:07:48 -0400

    Fixed LogIn page

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
 DROP DATABASE IF EXISTS beer_db;
 CREATE DATABASE beer_db;
+CREATE TABLE `beer`.`beer` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `pic` TEXT NULL,
+  `price` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
